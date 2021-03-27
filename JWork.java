f0ce8405b572a32145de6aaccb7915762a30b57f
Ivan <ivan.widjanarko@ui.ac.id>
2021-03-27 09:35:11 +0700

    Modul 3 - Post test - Edit Docs and PDF

--- JWork.java
+++ JWork.java
@@ -18,7 +18,7 @@ public class JWork
         Recruiter recruiterOne = new Recruiter(1806148706, "Ivan", "ivan@ui.ac.id", "08558069151", locationOne);
         Job jobOne = new Job(1806148706, "Programmer", recruiterOne, 10000000, JobCategory.WebDeveloper);
         Jobseeker jobseekerOne = new Jobseeker(1806148706, "Widjanarko", "widjanarko@ui.ac.id", "123456", "18-03-2021");
-        Invoice invoiceOne = new Invoice(1806148707, 369, "18-03-2021", 10000000, jobseekerOne, PaymentType.EwalletPayment, InvoiceStatus.Finished);
+        Invoice invoiceOne = new Invoice(100, jobOne.getId(), "18-03-2021", jobOne.getFee(), jobseekerOne, PaymentType.EwalletPayment, InvoiceStatus.Finished);
         
         System.out.println(recruiterOne.getName());
         recruiterOne.setName("Fajar");
